30d678ea211ecb97a0c8648e9cb1a2883e6838d9
Xuyang Li <dz21330015@smail.nju.edu.cn>
2022-12-15 22:56:27 +0800

    test

---
+++ test2.pluto.c
@@ -0,0 +1,6 @@
+#include <math.h>
+#define ceild(n,d)  (((n)<0) ? -((-(n))/(d)) : ((n)+(d)-1)/(d))
+#define floord(n,d) (((n)<0) ? -((-(n)+(d)-1)/(d)) : (n)/(d))
+#define max(x,y)    ((x) > (y)? (x) : (y))
+#define min(x,y)    ((x) < (y)? (x) : (y))
+
